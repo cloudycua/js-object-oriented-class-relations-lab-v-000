7f701ef91c07fb752a63d17aca86eb139050ccb5
Christine Cua <cloudycua@gmail.com>
2019-02-25 16:29:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,8 +16,8 @@ class Driver {
     });
   }
   passengers() {
-    return store.passengers.filter(passenger => {
-      return passenger.driverId == this.id;
+    return this.trips().map(trip => {
+      return trip.passenger();
     });
   }
 }
